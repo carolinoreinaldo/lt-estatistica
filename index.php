74fa94a5c1125490cd52653b9e9ab80e6c172f20
Fabricio Carolino <carolinoreinaldo@gmail.com>
2016-08-12 14:24:36 -0300

    Adição do bootstrap ao projeto

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LT-Estatistica</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+	<h1>Olá mundo!</h1>
+</body>
+</html>
